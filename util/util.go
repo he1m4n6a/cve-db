b27c1630173b37a5ba336111ade6bd128bcc60e3
he1m4n6a <he1m4n6a@163.com>
2020-04-13 23:34:12 +0800

    init

---
+++ util/util.go
@@ -0,0 +1,13 @@
+package util
+
+func GenWorkers(num int) chan<- func() {
+	tasks := make(chan func())
+	for i := 0; i < num; i++ {
+		go func() {
+			for f := range tasks {
+				f()
+			}
+		}()
+	}
+	return tasks
+}
